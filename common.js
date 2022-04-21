379d4b29c2a19991df11c30181d0867f8ac69321
Larissa <82511298+larissa-devweb@users.noreply.github.com>
2022-04-21 19:28:17 -0300

    Create common.js

---
+++ common.js
@@ -0,0 +1,7 @@
+function  isValidCommand ( comando )  { 
+  const  validCommands  =  [ 'sum' ] ; 
+
+  return  comandos válidos . inclui ( comando ) ; 
+} 
+
+módulo . export  =  { isValidCommand } ; 
